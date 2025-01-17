5f8aad595009928e026bf8d94badd57de58f2da6
mariamyasser <maruamyasser@mariam yasser>
2025-01-18 01:33:13 +0200

    todolist

--- src/app/component/footer/footer.component.ts
+++ src/app/component/footer/footer.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule,ReactiveFormsModule,NgClass],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+    private readonly _FormBuilder=inject(FormBuilder);
+    isloading:boolean=false;
 
+  sentdata:FormGroup=this._FormBuilder.group({
+  email:[null,[Validators.required, Validators.email]],
+  })  
+okfunc(){
+  this.isloading=true;
+if(this.sentdata.valid){
+  setTimeout(() => {
+    this.isloading=false;
+  }, 1000);
+this.sentdata.reset();
+} 
+else{
+  this.sentdata.markAllAsTouched();
+  setTimeout(() => {
+    this.isloading=false;
+  }, 1000);  console.log("error=",this.sentdata.value)
+}
+}
 }
